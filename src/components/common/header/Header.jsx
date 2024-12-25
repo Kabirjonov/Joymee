9f936a1d3e6b6c12c1d0d7d1e2f02db333b1a0cf
Oxunjon <ag9386581@gmail.com>
2024-12-26 03:41:36 +0500

    Decabr 26 03:41 navbar toggle padding qoshdim

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -101,7 +101,7 @@ export default function Header() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" style={{ background: 'var(--bone)' }} id="navbarSupportedContent">
+          <div className="collapse navbar-collapse p-3" style={{ background: 'var(--bone)' }} id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0 navbar__item-ul">
               {nav.map((list, index) => (
                 <li className="nav-item navbar__item-li" key={index}>
